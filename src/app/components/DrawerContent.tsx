b2a57b5f34d231e40c130657f6ea00dc774a0bf3
Arham Khan <sunnygeegee@gmail.com>
2023-11-12 08:01:28 -0800

    Update DrawerContent.tsx

--- src/app/components/DrawerContent.tsx
+++ src/app/components/DrawerContent.tsx
@@ -25,34 +25,30 @@ export const DrawerContent = ({
       }}>
       <ScrollView>
         <ProfileSection navigation={navigation} active={active} />
-        <Drawer.Section>
-          <Drawer.Item
-            label="Run"
-            icon="run"
-            active={active === 'Run'}
-            onPress={() => navigation.navigate('Run')}
-          />
-          <Drawer.Item
-            label="History"
-            icon="chart-bell-curve"
-            active={active === 'History'}
-            onPress={() => navigation.navigate('History')}
-          />
-        </Drawer.Section>
-        <Drawer.Section>
-          <Drawer.Item
-            label="Reminders"
-            icon="bell-circle-outline"
-            active={active === 'Reminders'}
-            onPress={() => navigation.navigate('Reminders')}
-          />
-          <Drawer.Item
-            label="Achievements"
-            icon="trophy-outline"
-            active={active === 'Achievements'}
-            onPress={() => navigation.navigate('Achievements')}
-          />
-        </Drawer.Section>
+        <Drawer.Item
+          label="Run"
+          icon="run"
+          active={active === 'Run'}
+          onPress={() => navigation.navigate('Run')}
+        />
+        <Drawer.Item
+          label="History"
+          icon="chart-bell-curve"
+          active={active === 'History'}
+          onPress={() => navigation.navigate('History')}
+        />
+        <Drawer.Item
+          label="Reminders"
+          icon="bell-circle-outline"
+          active={active === 'Reminders'}
+          onPress={() => navigation.navigate('Reminders')}
+        />
+        <Drawer.Item
+          label="Achievements"
+          icon="trophy-outline"
+          active={active === 'Achievements'}
+          onPress={() => navigation.navigate('Achievements')}
+        />
         <Drawer.Item
           label="Settings"
           icon="cog-outline"
